e726ceddc1bb43cbcb6985a8802a1d4844f2108d
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-01-31 22:39:51 +0100

    Initial Commit

---
+++ carStats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  carStats
+//
+//  Created by Ievgeniia Pavliuchyk on 31/01/2019.
+//  Copyright © 2019 Ievgeniia Pavliuchyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
