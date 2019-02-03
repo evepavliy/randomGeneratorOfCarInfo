c5aa50e0749f869b1fe86e19012a3e0ecf7bffcb
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-02-03 15:03:28 +0100

    Random car generator with data saved in plist file. Plist file is pre-loaded in the memory.  "Random car" button updates only "Image" atm.

--- carStats/ViewController.swift
+++ carStats/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBOutlet weak var label3: UILabel!
+    
+    @IBOutlet weak var label4: UILabel!
+    
+    @IBOutlet weak var label5: UILabel!
+    
+    @IBOutlet weak var label6: UILabel!
+    
+    var items = [[String:String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.items = loadPlist()
     }
 
+    func loadPlist() -> [[String:String]] {
+        
+        let path = Bundle.main.path(forResource: "carList", ofType: "plist")
+        return NSArray.init(contentsOf: URL.init(fileURLWithPath: path!)) as! [[String : String]]
+    
+    }
+    
+    
+    @IBAction func randomCar(_ sender: Any) {
+        
+        let dataSelect = items.randomElement()!
+        
+        imageView.image = UIImage(named: dataSelect["Image"]!)
+        
+    }
+    
+    
+    
 
 }
 
